9816e841b0adda0c0cdef8e4bd157f9e93b1c9dd
Anshu Dubey <142929668+git-anshudubey@users.noreply.github.com>
2025-12-12 19:49:53 +0000

    Started here

---
+++ Java/001_Hello_World.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is you name? ");
+        String name = sc.nextLine();
+
+        System.out.print("Hello "  + name + ", AWelcome in my Java DSA Practice.A ");
+    }
+}
