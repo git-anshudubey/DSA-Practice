d8979b0b884db5846320ca8504bf453acc319087
Anshu Dubey <142929668+git-anshudubey@users.noreply.github.com>
2025-12-14 20:06:35 +0530

    DSA practice

---
+++ Java/002_Taking_Array_as_Input.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array: ")
+        int size = sc.nextInt();
+
+        int array[] = new int[size];
+
+        System.out.println("Enter the elements of the array: ");
+        for(int i = 0; i < size; i++){
+            arrray[size] = sc.nextInt();
+        }
+
+        System.out.println("Array: ");
+        for(int i = 0; i < size; i++){
+            System.out.println(arr[i] + " ");
+        }
+    }
+}
