c1f9f7b40fa94bf05c716f21b60609950b457f39
Anshu Dubey <142929668+git-anshudubey@users.noreply.github.com>
2025-12-14 20:14:58 +0530

    practice
    Fixed array input and output logic in the program.

--- Java/002_Taking_Array_as_Input.java
+++ Java/002_Taking_Array_as_Input.java
@@ -4,19 +4,19 @@ class Solution {
     public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of array: ")
+        System.out.print("Enter the size of array: ");
         int size = sc.nextInt();
 
         int array[] = new int[size];
 
         System.out.println("Enter the elements of the array: ");
         for(int i = 0; i < size; i++){
-            arrray[size] = sc.nextInt();
+            array[i] = sc.nextInt();
         }
 
-        System.out.println("Array: ");
+        System.out.print("Array is: ");
         for(int i = 0; i < size; i++){
-            System.out.println(arr[i] + " ");
+            System.out.print(array[i] + " ");
         }
     }
 }
